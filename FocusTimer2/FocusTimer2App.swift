4a9df9133b11c02ae62a3d61eace7aabe0131dc2
Dostan <turlybekdostan@gmail.com>
2025-04-06 13:32:33 +0500

    Initial Commit

---
+++ FocusTimer2/FocusTimer2App.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimer2App.swift
+//  FocusTimer2
+//
+//  Created by Dostan Turlybek on 06.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct FocusTimer2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
