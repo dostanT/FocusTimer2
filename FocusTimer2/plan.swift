bf1dddee7fd417868c9104f3edd67234bbb26c3c
Dostan <turlybekdostan@gmail.com>
2025-04-15 21:21:29 +0500

    0.0.0.5.2

--- FocusTimer2/plan.swift
+++ FocusTimer2/plan.swift
@@ -8,4 +8,5 @@
  14April2025
  Need to change color pallete to white and black for readable
  need to create perehod in sets
+ use Form And Section for History and sets. Its reale easy slove for my problem with HIG!
  */
